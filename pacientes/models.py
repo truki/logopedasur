ec75cb81ba9d175a02c4939f21b1df50354dacf3
trukise <trukise@gmail.com>
2016-11-21 16:24:11 +0100

    Creada primer version de modelo Pacientes y superuser trukise

---
+++ pacientes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Paciente(models.Model):
+    nombre      = models.CharField(max_length=128, blank=False)
+    apellidos   = models.CharField(max_length=256, blank=True)
+    dni         = models.CharField(max_length=9, unique=True)
+    direccion   = models.CharField(max_length=256, blank=False)
+
+    def __str__(self):
+        return self.nombre + self.apellidos
