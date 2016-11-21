59e36e8ca00c4460b85eb125e22e7ce98b810ff8
trukise <trukise@gmail.com>
2016-11-21 16:33:58 +0100

    Se añade Paciente al backend de administración de Django

--- pacientes/models.py
+++ pacientes/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Paciente(models.Model):
-    nombre      = models.CharField(max_length=128, blank=False)
-    apellidos   = models.CharField(max_length=256, blank=True)
-    dni         = models.CharField(max_length=9, unique=True)
-    direccion   = models.CharField(max_length=256, blank=False)
+
+    nombre = models.CharField(max_length=128, blank=False)
+    apellidos = models.CharField(max_length=256, blank=True)
+    dni = models.CharField(max_length=9, unique=True)
+    direccion = models.CharField(max_length=256, blank=True)
 
     def __str__(self):
-        return self.nombre + self.apellidos
+        return self.nombre + " " + self.apellidos
