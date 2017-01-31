c77377697736bc15801dfb7b52ae31ae50d9e8d2
trukise <trukise@gmail.com>
2017-01-31 12:39:52 +0100

    Modificaciones en template terapeutas_index.html para mostrar el listado de terapeutas.

--- pacientes/urls.py
+++ pacientes/urls.py
@@ -3,7 +3,7 @@ from pacientes import views
 
 app_name = 'pacientes'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='pacientes_index'),
     url(r'^(?P<pk>\d+)/$', views.paciente_detail, name='paciente_detail'),
     url(r'sesion/^(?P<pk>\d+)/$', views.sesion_detail, name='sesion_detail'),
 
