a0605037a8389700377606c55cdefa62a24ea71f
Sergio Sánchez <trukise@gmail.com>
2017-02-07 16:58:05 +0100

    Added pagination to therapeutas list

--- terapeutas/views.py
+++ terapeutas/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
@@ -14,6 +15,19 @@ def index(request):
         Vista para mostrar el listado de terapeutas
     '''
     terapeutas = Terapeuta.objects.all()
+    paginator = Paginator(terapeutas, 8) # show 8 therapeutas
+
+    page = request.GET.get('page')
+    try:
+        terapeutas = paginator.page(page)
+    except PageNotAnInteger:
+        # si pagina no es un entero posicionamos en la primera
+        terapeutas = paginator.page(1)
+    except:
+        # si la pagina está fuera de rango por arriba, nos
+        # posicionamos en la última
+        terapeutas = paginator.page(paginator.num_pages)
+
     return render(request, 'terapeutas_index.html', {"terapeutas": terapeutas})
 
 
